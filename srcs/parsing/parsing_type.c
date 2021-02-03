43ddc5011883e98ed46493eb1987874e4a142c7d
Nastasia DEMONT <ndemont@e2r5p18.42.fr>
2021-02-03 13:19:06 +0100

    Error parsing for lights

--- srcs/parsing/parsing_type.c
+++ srcs/parsing/parsing_type.c
@@ -6,17 +6,33 @@
 /*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 13:19:18 by ndemont           #+#    #+#             */
-/*   Updated: 2021/02/03 11:25:49 by ndemont          ###   ########.fr       */
+/*   Updated: 2021/02/03 13:13:17 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+
+char 		*leaks(char **split, char *error)
+{
+	int i;
+
+	i = 0;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+	return (error);
+}
 
 int			ft_strisfloat(char *str)
 {
 	int i;
 
 	i = 0;
+	if (str[i] == '-')
+		i++;
 	while (ft_isdigit(str[i]))
 		i++;
 	if (str[i] == '.')
@@ -58,64 +74,60 @@ int			ft_count_coma(char *str)
 	return (1);
 }
 
-char		*ft_check_coord(char *line, float *x, float *y, float *z)
+char		*ft_check_coord(char *line, float c[3])
 {
 	char	**split;
+	int 	i;
 
 	if (!(ft_count_coma(line)))
 		return ("Wrong arguments format");
 	split = ft_split(line, ',');
-	if (ft_strisfloat(split[0]))
-		*x = ft_atof(split[1]);
-	else
-		return ("Color value must be float");
-	if (ft_strisfloat(split[1]))
-		*y = ft_atof(split[2]);
-	else
-		return ("Color value must be float");
-	if (ft_strisfloat(split[2]))
-		*z = ft_atof(split[2]);
-	else
-		return ("Color value must be float");
+	i = 0;
+	while (i < 3)
+	{
+		if (ft_strisfloat(split[i]))
+			c[i] = ft_atof(split[i]);
+		else
+			return (leaks(split, "Color value must be float"));
+		i++;
+	}
 	if (split[3])
-		return ("Wrong number of coordinates arguments");
-	return (0);
+		return (leaks(split, "Wrong number of coordinates arguments"));
+	return (leaks(split, 0));
 }
 
-char		*ft_check_color(char *line, float *coord[3])
+char		*ft_check_color(char *line, float c[3])
 {
 	char	**split;
+	int		i;
 
 	if (!(ft_count_coma(line)))
 		return ("Wrong arguments format");
 	split = ft_split(line, ',');
-	if (ft_strisdigit(split[0]))
-		*coord[0] = ft_atof(split[1]);
-	else
-		return ("Color value must be integer");
-	if (ft_strisdigit(split[1]))
-		*coord[1] = ft_atof(split[2]);
-	else
-		return ("Color value must be integer");
-	if (ft_strisdigit(split[2]))
-		*coord[2] = ft_atof(split[2]);
-	else
-		return ("Color value must be integer");
+	i = 0;
+	while (i < 3)
+	{
+		if (ft_strisdigit(split[i]))
+			c[i]= ft_atof(split[i]);
+		else 
+			return (leaks(split, "Color value must be integer"));
+		i++;
+	}
 	if (split[3])
-		return ("Wrong number of color arguments");
-	//if (*x < 0 || *y < 0 || *z < 0 || *x > 255 || *y > 255 || *z > 255)
-	//	return ("Color values must ∈ [0;255]");
-	return (0);
+		return (leaks(split, "Wrong number of color arguments"));
+	if (c[0] < 0 || c[1] < 0 || c[2] < 0 || c[0] > 255 || c[1] > 255 || c[2] > 255)
+		return (leaks(split, "Color values must ∈ [0;255]"));
+	return (leaks(split, 0));
 }
 
 char		*parsing_r(char **line, t_scene *elem)
 {
 	int i;
 
-	i = 0;
+	i = 1;
 	while (i < 3)
 	{
-		if (!ft_strisfloat(line[1]))
+		if (!ft_strisfloat(line[i]))
 			return ("Resolution: value must be digital.\n");
 		i++;
 	}
@@ -134,7 +146,7 @@ char		*parsing_r(char **line, t_scene *elem)
 
 char		*parsing_a(char **line, t_scene *elem)
 {
-	char	**split;
+	char 	*error;
 	int		i;
 
 	if (!ft_strisfloat(line[1]))
@@ -142,25 +154,18 @@ char		*parsing_a(char **line, t_scene *elem)
 	elem->A.i = ft_atof(line[1]);
 	if (!(ft_count_coma(line[2])))
 		return ("Ambiant light: wrong arguments format");
-	split = ft_split(line[2], ',');
 	i = 0;
-	while (i < 3)
-	{
-		if (!ft_strisdigit(split[i]))
-			return ("Ambiant light: color values must be digital.");
-		elem->A.color.coord[i] = ft_atof(split[i]);
-		if ((int)elem->A.color.coord[i] < 0 || (int)elem->A.color.coord[i] > 255)
-			return ("Ambiant light: color values must ∈ [0;255]");
-		i++;
-	}
-	if (line[3] || split[i])
+	if ((error = ft_check_color(line[2], elem->A.color.coord)))
+			return (error);
+	if (line[3])
 		return ("Ambiant light: too many arguments.");
 	return (0);
 }
 
 char		*parsing_c(char **line, t_scene *elem)
 {
-	char	**split;
+	//char	**split;
+	char	*error;
 	int		i;
 
 	if (line[4])
@@ -168,14 +173,10 @@ char		*parsing_c(char **line, t_scene *elem)
 	i = 0;
 	while (elem->cameras[i].f != -1)
 		i++;
-	split = ft_split(line[1], ',');
-	elem->cameras[i].o.coord[0] = ft_atof(split[0]);
-	elem->cameras[i].o.coord[1] = ft_atof(split[1]);
-	elem->cameras[i].o.coord[2] = ft_atof(split[2]);
-	split = ft_split(line[2], ',');
-	elem->cameras[i].c.coord[0] = ft_atof(split[0]);
-	elem->cameras[i].c.coord[1] = ft_atof(split[1]);
-	elem->cameras[i].c.coord[2] = ft_atof(split[2]);
+	if ((error = ft_check_coord(line[1], elem->cameras[i].o.coord)))
+		return (error);
+	if ((error = ft_check_coord(line[2], elem->cameras[i].c.coord)))
+		return (error);
 	elem->cameras[i].f = ft_atof(line[3]) * (M_PI / 180);
 	i++;
 	elem->cameras[i].f = -1;
@@ -184,7 +185,7 @@ char		*parsing_c(char **line, t_scene *elem)
 
 char		*parsing_l(char **line, t_scene *elem)
 {
-	char	**split;
+	char	*error;
 	int		i;
 
 	if (line[4])
@@ -192,19 +193,14 @@ char		*parsing_l(char **line, t_scene *elem)
 	i = 0;
 	while (elem->lights[i].i != -1)
 		i++;
-	split = ft_split(line[1], ',');
-	elem->lights[i].o.coord[0] = ft_atof(split[0]);
-	elem->lights[i].o.coord[1] = ft_atof(split[1]);
-	elem->lights[i].o.coord[2] = ft_atof(split[2]);
+	if ((error = ft_check_coord(line[1], elem->lights[i].o.coord)))
+		return (error);
 	elem->lights[i].i = ft_atof(line[2]);
-	split = ft_split(line[3], ',');
-	elem->lights[i].c.coord[0] = ft_atof(split[0]);
-	elem->lights[i].c.coord[1] = ft_atof(split[1]);
-	elem->lights[i].c.coord[2] = ft_atof(split[2]);
+	if ((error = ft_check_coord(line[3], elem->lights[i].c.coord)))
+		return (error);
 	i++;
 	elem->lights[i].i = -1;
 	return (0);
-
 }
 
 char		*parsing_pl(char **line, t_scene *elem)
